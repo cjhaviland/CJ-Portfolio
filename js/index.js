b854b94f10e8d520dd7b5b0ab993726cda397775
cjhaviland <cj.haviland@yahoo.com>
2017-11-24 11:20:26 -0500

    Changes from Codepen in html, css, and js

--- js/index.js
+++ js/index.js
@@ -2,28 +2,30 @@ $(function(){
   
   // Function to create HTML for Skills section
   function buildSkills(){
-    var html = '';
+    let html = '';
     
     // Label object for skill level lookup
-    var skillLabel = {
-      0 : 'Jon Snow...', 
-      1 :'Beginner', 
-      2 : 'Intermediate', 
+    let skillLabel = {
+      0 : 'Novice', 
+      1 : 'Intermediate', 
+      2 : 'Advanced', 
       3 : 'Expert'
     };
 
     // To cut down on the verbose HTML, automate it
-    var skills = [
-      {skill:'HTML5, CSS3', level: 2},
+    let skills = [
+      {skill:'HTML5, CSS3', level: 1},
       {skill:'Javascript', level: 2},
+      {skill:'Node.js', level: 1},
+      {skill:'React.js', level: 0},
       {skill:'SQL', level: 2},
       {skill:'Python', level: 1},
-      {skill:'C', level: 2},
-      {skill:'Node.js', level: 0}
+      {skill:'C', level: 1},
+      {skill:'Knitting', level: 3}
     ];
 
     // Build HTML for Skills section
-    for (var i in skills){
+    for (let i in skills){
       html += '<div class="row"><div class="col-md-6 text-left">'+
       skills[i]['skill'] +
       '</div><div class="col-md-6 text-right">' +
@@ -34,13 +36,13 @@ $(function(){
     }
     
     return html;
-  };
+  }; // End of buildSkills
   
   $('#skills-list').append(buildSkills());
   
   
   // Build Modal HTML from Object
-  var examples = {
+  let examples = {
     'bingo' : [
       { 'title' : 'Interactive Bingo Board <a href="#" role="button" class="btn btn-sm btn-secondary disabled" aria-disabled="true">HTML</a> <a href="#" role="button" class="btn btn-sm btn-secondary disabled" aria-disabled="true">JavaScript</a>',
         'study' : '<p><u>Project</u>: Personal</p><p><u>Objective</u>:  After seeing an image of "Conference Call Bingo" going around on the internet I decided to code it for fun!</p><p><u>Process</u>:  Created an array of entries for the Bingo boxes that are then randomized each time the table is loaded. Made each box clickable, which highlights the selection. </p><p><u>Result</u>:  Click below to view screencaps.</p>', 
@@ -67,46 +69,58 @@ $(function(){
                    'http://camhaviland.com/img/portfolio_examples/mashup_3.png',
                    'http://camhaviland.com/img/portfolio_examples/mashup_4.png']}
     ],
-    'sigConsole' : [
-      { 'title' : 'FTP Console App <a href="#" role="button" class="btn btn-sm btn-secondary disabled" aria-disabled="true">C#</a>',
-        'study' : '<p><u>Project</u>: Sigmund Software</p><p><u>Objective</u>: Automate file transfers for client software updates.</p><p><u>Process</u>: One of my responsibilities at Sigmund Software is performing regular software updates for our non-hosted clients. Downloading new files every time was becoming tedious so I wrote a C# console app using the FluentFTP client package to automate the process. The challenge I faced was downloading the entire contents of the directory (including subdirectories) securely. To solve this issue I implemented a recursive function that will create the directories if they don\'t exist, then enter that directory and repeat, otherwise just download the files.</p>', 
-       'images' : ['http://camhaviland.com/img/portfolio_examples/sigmund_logo_thumb.png']}
+    'sigPortal' : [
+      { 'title' : 'Customer Access Portal <a href="#" role="button" class="btn btn-sm btn-secondary disabled" aria-disabled="true">C#</a>',
+        'study' : '<p><u>Project</u>: Sigmund Software</p><p><u>Objective</u>: Write a portal for customers to access training documentation and help ticket statuses.</p><p><u>Process</u>: Recreated a previously existing ASP.NET website using using MVC and .NET Framework to create a new web application. The website needed to authenticate logins as well as display and filter data from several databases.</p>', 
+       'images' : ['http://camhaviland.com/img/portfolio_examples/ErrorPage.png', 
+                  'http://camhaviland.com/img/portfolio_examples/Home Page.png',
+                  'http://camhaviland.com/img/portfolio_examples/ReleaseNotes.png',
+                  'http://camhaviland.com/img/portfolio_examples/SigServerStatus.png']}
     ],
     'sigIp' : [
       { 'title' : 'Backend IP Restriction <a href="#" role="button" class="btn btn-sm btn-secondary disabled" aria-disabled="true">C#</a> <a href="#" role="button" class="btn btn-sm btn-secondary disabled" aria-disabled="true">SQL</a> <a href="#" role="button" class="btn btn-sm btn-secondary disabled" aria-disabled="true">MVC</a>',
         'study' : '<p><u>Project</u>: Sigmund Software</p><p><u>Objective</u>:  Create IP restrictions that deter users from accessing patient data outside of the facility.</p><p><u>Process</u>: Utilizing my networking background I was assigned to create a new feature to Sigmund’s web version, Aura. I used C# to create a backend IP restriction which allows some of our clients to regulate where their users log into the software in order to comply with HIPAA regulations.</p>', 
        'images' : ['http://camhaviland.com/img/portfolio_examples/sigmund_logo_thumb.png']}
+    ],
+    'ronSwanson' : [
+      { 'title' : 'Ron Swanson Random Quote Generator <a href="#" role="button" class="btn btn-sm btn-secondary disabled" aria-disabled="true">HTML</a> <a href="#" role="button" class="btn btn-sm btn-secondary disabled" aria-disabled="true">CSS</a> <a href="#" role="button" class="btn btn-sm btn-secondary disabled" aria-disabled="true">Javascript</a>',
+        'study' : '<p><u>Project</u>: Udemy - Advanced Developer Bootcamp</p><p><u>Objective</u>:  Use a single API endpoint four different ways.</p><p><u>Process</u>: Starting with the vanilla way to use an API endpoint with XHR I created a way to access the random quote generator. In the class we also reviewed using Fetch, jQuery, and Axios as ways to use an API. These were also implemented so each button makes a call to the API using one of those libraries. Each library was presented with it\'s positives and negatives running on various browsers.</p>', 
+       'images' : ['http://camhaviland.com/img/portfolio_examples/ronswanson.png']}
+    ],
+    'timestamp' : [
+      { 'title' : 'Timestamp Microservice <a href="#" role="button" class="btn btn-sm btn-secondary disabled" aria-disabled="true">Node.js</a> <a href="#" role="button" class="btn btn-sm btn-secondary disabled" aria-disabled="true">Express</a>',
+        'study' : '<p><u>Project</u>: FreeCodeCamp</p><p><u>Objective</u>:  Return a JSON object of UNIX and English timestamp based on passed input.</p><p><u>Process</u>: Original user stories asked you check if the parameter passed is UNIX or some form of English, but I wanted to go a step further. Found a third party library which accepts strings like today or tomorrow or December as possible input.</p><p><u>Link: </u><a class="case-study-link" href="https://elderly-innocent.glitch.me/">Timestamp Microservice</a></p>', 
+       'images' : ['http://camhaviland.com/img/portfolio_examples/timestamp_1.png', 'http://camhaviland.com/img/portfolio_examples/timestamp_2.png', 'http://camhaviland.com/img/portfolio_examples/timestamp_3.png']}
     ]
   };
   
   // Build the modal with carousel when you click on an image
   $('.overlay').on('click', function(){
-    $('#myModal').on('show.bs.modal', function(event){
-      var ex = $(event.relatedTarget);
-      var name = ex.data('name');
+    $('#myModal').on('show.bs.modal', function(e){
       
-      var modal = $(this);
+      let ex = $(e.relatedTarget);
+      let name = ex.data('name');
+      
+      let modal = $(this);
       
       // Fill the modals carousel with images once Modal is shown
-      var images = examples[name][0]['images'];
+      let images = examples[name][0]['images'];
       
-      $('#myModal').on('shown.bs.modal', function(event){
+      $('#myModal').on('shown.bs.modal', function(){
         // Build Images for carousel inner from object
-        var html = '';
-        for (var i = 0 ; i < images.length ; i++) {
-          html += '<div class="carousel-item"><img class="img-fluid mx-auto" src="'+images[i]+'"></div>';
+        let html = '';
+        for (let i = 0 ; i < images.length ; i++) {
+          html += '<div class="carousel-item"><img id="modal-image" src="'+images[i]+'"></div>';
         }
         $('.carousel-inner').html(html); // Slap it into the DOM
         $('.carousel-item').first().addClass('active'); // Set the first image as active
         $('.modal-title').html(examples[name][0]['title']);
         $('#study-text').html(examples[name][0]['study']);
-      });
-      
-      
+      });  
     });
 	
 	// Once modal is closed clear out the old images
-	$('#myModal').on('hidden.bs.modal', function(event){
+	$('#myModal').on('hidden.bs.modal', function(){
 		$('.modal-title').empty();
     $('#study-text').empty();
     $('.carousel-inner').empty();
